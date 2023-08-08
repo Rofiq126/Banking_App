1e9d95d936a212c067c8fd4e70a4de468f41c9b6
Rofiqul126 <rofiqulumma@gmail.com>
2023-08-08 16:03:37 +0700

    add data model for dropDown in add card screen

---
+++ lib/model/list_date.dart
@@ -0,0 +1,9 @@
+class Tanggal {
+  List date = <String>['1', '2', '3', '4', '5'];
+
+  List month = <String>['Januari', 'Februari', 'Maret', 'April', 'Mei'];
+
+  List year = <String>['2000', '2001', '2002', '2003', '2004', '2005'];
+
+  List phone = <String>['+62'];
+}
